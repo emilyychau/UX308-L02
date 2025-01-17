d5498603c21fff0809cea620ad79d181efe0507a
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-01-17 00:45:01 +0000

    create in class.js

---
+++ in class.js
@@ -0,0 +1,10 @@
+// Take a variable with distance in **inches** (in) as input and outputs the same distance but *converted* into **feet** (ft)
+// verbs = what the program does
+
+let inches = 5;
+
+let feet = inches/12; 
+
+console.log(`${inches} inches is ${feet} feet`);
+
+// run program in terminal: $ node [fileName.js]
---
+++ question1.js
@@ -0,0 +1,5 @@
+// Take a variable with distance in kilometres as input and output the same distance converted to miles
+
+let inputKM = prompt("Enter a distance in kilometers?")
+var convMiles = 
+console.log(convMiles);
