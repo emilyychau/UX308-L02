21c893d42b7df1ca47f61a4ea2a108eb204bcbe6
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-01-17 01:49:58 +0000

    edit question 2 outputs

--- question2.js
+++ question2.js
@@ -2,16 +2,16 @@
 
 let percentage = 24;
 
-if (percentage >= 90){console.log("A+")}
-else if (percentage >= 85 && percentage <= 89){console.log("A")}
-else if (percentage >= 80 && percentage <= 84){console.log("A-")}
-else if (percentage >= 77 && percentage <= 79){console.log("B+")}
-else if (percentage >=73 && percentage <= 76){console.log("B")}
-else if (percentage >=70 && percentage <= 72){console.log("B-")}
-else if (percentage >=67 && percentage <= 69){console.log("C+")}
-else if (percentage >=63 && percentage <= 66){console.log("C")}
-else if (percentage >=60 && percentage <= 62){console.log("C-")}
-else if (percentage >=57 && percentage <= 59){console.log("D+")}
-else if (percentage >=53 && percentage <= 56){console.log("D")}
-else if (percentage >=50 && percentage <= 52){console.log("D-")}
-else (percentage >=0 && percentage <= 49){console.log("F")}
+if (percentage >= 90){console.log("Your letter grade is an A+")}
+else if (percentage >= 85 && percentage <= 89){console.log("Your letter grade is an A")}
+else if (percentage >= 80 && percentage <= 84){console.log("Your letter grade is an A-")}
+else if (percentage >= 77 && percentage <= 79){console.log("Your letter grade is a B+")}
+else if (percentage >=73 && percentage <= 76){console.log("Your letter grade is a B")}
+else if (percentage >=70 && percentage <= 72){console.log("Your letter grade is a B-")}
+else if (percentage >=67 && percentage <= 69){console.log("Your letter grade is a C+")}
+else if (percentage >=63 && percentage <= 66){console.log("Your letter grade is a C")}
+else if (percentage >=60 && percentage <= 62){console.log("Your letter grade is a C-")}
+else if (percentage >=57 && percentage <= 59){console.log("Your letter grade is a D+")}
+else if (percentage >=53 && percentage <= 56){console.log("Your letter grade is a D")}
+else if (percentage >=50 && percentage <= 52){console.log("Your letter grade is a D-")}
+else if (percentage >=0 && percentage <= 49){console.log("Your letter grade is an F")}
